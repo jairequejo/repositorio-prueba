e1457cd44217fb251b8664ddf67d81d2256ee823
jairequejo <jairequejo@gmail.com>
2025-07-25 17:19:03 -0500

    hola a secas

--- codigo.py
+++ codigo.py
@@ -2,5 +2,5 @@ nombre = input("Coloca tu nombre: ")
 apellido = input("Coloca tu apellido: ")
 edad = input("Coloca tu edad: ")
 
-saludo = "Hola crack maquinola y no se que mas eres hermano " + nombre +  " " + apellido +". " + "Tu edad es: " + edad
+saludo = "Hola " + nombre +  " " + apellido +". " + "Tu edad es: " + edad
 print(saludo)
