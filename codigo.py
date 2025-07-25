1cbd23ea2bf33c8f8ef5009aca6be36ac26e76eb
Jair Requejo <jairequejo@gmail.com>
2025-07-25 16:36:31 -0500

    edad codigo.py

--- codigo.py
+++ codigo.py
@@ -1,5 +1,6 @@
 nombre = "Anderson Jair"
 apellido = "Requejo Sifuentes"
+edad = 19
 
-saludo = "Hola " + nombre +  " " + apellido +"."
+saludo = "Hola " + nombre +  " " + apellido +". " + edad 
 print(saludo)
